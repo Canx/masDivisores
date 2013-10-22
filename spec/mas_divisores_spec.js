1d394d7af471be6908b514e8bf4517a13a14b93c
Ruben Cancho <canchete@gmail.com>
2013-10-22 23:09:40 +0200

    Improved module loading

--- spec/mas_divisores_spec.js
+++ spec/mas_divisores_spec.js
@@ -1,25 +1,22 @@
-var mas_divisores = require('../src/mas_divisores').mas_divisores;
-var total_divisores = require('../src/mas_divisores').total_divisores;
-var factores_primos = require('../src/mas_divisores').factores_primos;
-var resumir = require('../src/mas_divisores').resumir;
+var MD = require('../src/mas_divisores');
 
 describe("mas_divisores", function() {
   it("returns 1 with 1 divisor with max = 1", function() {
-    var max = mas_divisores(1);
+    var max = MD.mas_divisores(1);
     expect(max.numero).toEqual(1);
     expect(max.total).toEqual(1);
     expect(max.factores).toEqual([1]);
   });
 
   it("returns 10 with 4 divisors with max = 10", function() {
-    var max = mas_divisores(10);
+    var max = MD.mas_divisores(10);
     expect(max.numero).toEqual(10);
     expect(max.total).toEqual(4);
     expect(max.factores).toEqual([1,2,5,10]);
   });
 
   it("returns 96 with 12 divisors with max = 100", function() {
-    var max = mas_divisores(100);
+    var max = MD.mas_divisores(100);
     expect(max.numero).toEqual(96);
     expect(max.total).toEqual(12);
     expect(max.factores).toEqual([1,2,3,4,6,8,12,16,24,32,48,96]);
@@ -27,7 +24,7 @@ describe("mas_divisores", function() {
 
   it("should calc max = 10000 in less than 2000 miliseconds", function() {
     var t_ini = (new Date).getTime();
-    var max = mas_divisores(10000);
+    var max = MD.mas_divisores(10000);
     var t_fin = (new Date).getTime();
     var t = t_fin - t_ini;
     expect(t).toBeLessThan(2000);
@@ -37,23 +34,23 @@ describe("mas_divisores", function() {
 
 describe("total_divisores", function() {
   it("returns correct number of total divisors", function() {
-    expect(total_divisores(10)).toEqual(4);
-    expect(total_divisores(18)).toEqual(6);
-    expect(total_divisores(24)).toEqual(8);
+    expect(MD.total_divisores(10)).toEqual(4);
+    expect(MD.total_divisores(18)).toEqual(6);
+    expect(MD.total_divisores(24)).toEqual(8);
   });
 });
 
 describe("factores_primos", function() {
   it("returns an array with all prime factores", function() {
-    expect(factores_primos(10).sort()).toEqual([2,5]);
-    expect(factores_primos(18).sort()).toEqual([2,3,3]);
-    expect(factores_primos(24).sort()).toEqual([2,2,2,3]);
+    expect(MD.factores_primos(10).sort()).toEqual([2,5]);
+    expect(MD.factores_primos(18).sort()).toEqual([2,3,3]);
+    expect(MD.factores_primos(24).sort()).toEqual([2,2,2,3]);
   });
 });
 
 describe("resumir", function() {
   it("returns a resume counting objects from array", function() {
-    expect(resumir([2,5])).toEqual([[2,1],[5,1]]);
-    expect(resumir([2,2,2,3])).toEqual([[2,3],[3,1]]);
+    expect(MD.resumir([2,5])).toEqual([[2,1],[5,1]]);
+    expect(MD.resumir([2,2,2,3])).toEqual([[2,3],[3,1]]);
   });
 });
--- src/mas_divisores.js
+++ src/mas_divisores.js
@@ -1,105 +1,110 @@
-// calcular el número con más divisores desde 1 hasta tope
-function mas_divisores(tope) {
+module.exports = function() {
+
+  // calcular el número con más divisores desde 1 hasta tope
+  function mas_divisores(tope) {
     var f = 1;
     var f_nuevo;
     var n = 1;
     var i = 1;
-
+  
     while(i<=tope) {
       f_nuevo = total_divisores(i);
-
+  
       if (f_nuevo >= f) {
           f = f_nuevo;
           n = i;
       }
       i++;
     }
-
+  
     return {
        numero: n,
        factores: divisores(n),
        total: f
     }
-}
+  }
 
-// calcular los divisores de num
-function divisores(num) {
-  var resumido = resumir(factores_primos(num))
+  // calcular los divisores de num
+  function divisores(num) {
+    var i;
+    var divisores = [];
+    var temp_divisores;
+    var exponente;
+    var resumido = resumir(factores_primos(num))
 
-  var i;
-  var divisores = [];
-  var temp_divisores;
-  var exponente;
+    resumido.forEach(function(factor_primo) {
+      temp_divisores = [];
+      for(exponente = 1; exponente <= factor_primo[1]; exponente++) {
+        divisores.forEach(function(divisor) {
+          temp_divisores.push(divisor*Math.pow(factor_primo[0],exponente));
+        });
+        temp_divisores.push(Math.pow(factor_primo[0],exponente));
+      }
+      divisores = divisores.concat(temp_divisores);
+    });
 
-  resumido.forEach(function(factor_primo) {
-    temp_divisores = [];
-    for(exponente = 1; exponente <= factor_primo[1]; exponente++) {
-      divisores.forEach(function(divisor) {
-        temp_divisores.push(divisor*Math.pow(factor_primo[0],exponente));
-      });
-      temp_divisores.push(Math.pow(factor_primo[0],exponente));
-    }
-    divisores = divisores.concat(temp_divisores);
-  });
+    divisores.push(1);
 
-  divisores.push(1);
+    return divisores.sort(function(a,b) {return a - b;});
+  }
 
-  return divisores.sort(function(a,b) {return a - b;});
-}
-
-// a partir de los factores primos obtenemos cuantos divisores hay
-function total_divisores(num) {
-  var f = 1;
+  // a partir de los factores primos obtenemos cuantos divisores hay
+  function total_divisores(num) {
+    var f = 1;
+    
+    resumir(factores_primos(num))
+    .forEach(function(element, index, array) {
+      f = f * (element[1] + 1);
+    });
+    return f;
+  }
   
-  resumir(factores_primos(num))
-  .forEach(function(element, index, array) {
-    f = f * (element[1] + 1);
-  });
-  return f;
-}
+  // convertimos [2,2,2,3,3,5] en [[2,3],[3,2],[5,1]]
+  function resumir(array) {
+    var last;
+    var resumido = array.reduce(function(previous, current) {
+      last = previous.slice(-1)[0];
+      if (last[0] == current) {
+          last[1] = last[1] + 1;
+      } else {
+          previous.push([current,1]);
+      }
+      return previous;
+    }, [[1,1]]);
+    resumido.shift();
+    return resumido;
+  }
 
-// convertimos [2,2,2,3,3,5] en [[2,3],[3,2],[5,1]]
-function resumir(array) {
-  var last;
-  var resumido = array.reduce(function(previous, current) {
-    last = previous.slice(-1)[0];
-    if (last[0] == current) {
-        last[1] = last[1] + 1;
-    } else {
-        previous.push([current,1]);
-    }
-    return previous;
-  }, [[1,1]]);
-  resumido.shift();
-  return resumido;
-}
-
-// devuelve un array con los factores primos del número solicitado
-function factores_primos(num) {
-  var factores = [];
-  var posible_divisor = 2;
-  var division;
-
-  while (posible_divisor <= Math.sqrt(num)) {
-    while (es_entero(division = num / posible_divisor)) {
-      factores.push(posible_divisor);
-      num = division;
+  // devuelve un array con los factores primos del número solicitado
+  function factores_primos(num) {
+    var factores = [];
+    var posible_divisor = 2;
+    var division;
+  
+    while (posible_divisor <= Math.sqrt(num)) {
+      while (es_entero(division = num / posible_divisor)) {
+        factores.push(posible_divisor);
+        num = division;
+      };
+      posible_divisor = posible_divisor + 1;
     };
-    posible_divisor = posible_divisor + 1;
-  };
+  
+    if (num!= 1) factores.push(num);
+  
+    return factores;
+  }
+  
+  // devuelve verdadero si tiene decimales
+  function es_entero(num) {
+      return num % 1 == 0
+  }
 
-  if (num!= 1) factores.push(num);
+  return {
+    mas_divisores: mas_divisores,
+    divisores: divisores,
+    total_divisores: total_divisores,
+    resumir: resumir,
+    factores_primos: factores_primos
+  }
 
-  return factores;
-}
-
-// devuelve verdadero si tiene decimales
-function es_entero(num) {
-    return num % 1 == 0
-}
-
-exports.mas_divisores = mas_divisores;
-exports.total_divisores = total_divisores;
-exports.divisores = divisores;
-exports.factores_primos = factores_primos;
-exports.resumir = resumir;
+}();
